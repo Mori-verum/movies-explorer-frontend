edae162b6617eea4b83c665938055f910ba37326
Selivokhina Anastasia <nastya.selivokhina8@gmail.com>
2023-06-10 12:20:06 +0300

    added saved-movies page

--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -1,5 +1,17 @@
-function SavedMovies() {
+import MoviesCardList from "../MoviesCardList/MoviesCardList";
+import Search from "../Search/Search";
 
+import cards from '../../utils/cards'
+import MoreMovies from "../MoreMovies/MoreMovies";
+
+function SavedMovies() {
+    return (
+        <main className="main">
+            <Search />
+            <MoviesCardList cards = { cards } />
+            <MoreMovies />
+        </main>
+    )
 }
 
 export default SavedMovies;
